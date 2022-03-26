28ee2efbcb564f37bca1f3afe65c5f977da04e1a
DharunVG <101557453+DharunVG@users.noreply.github.com>
2022-03-26 14:37:45 +0530

    Update Calculate Shortest Distance Between Two Points.java

--- Calculate Shortest Distance Between Two Points.java
+++ Calculate Shortest Distance Between Two Points.java
@@ -5,8 +5,6 @@ public class shortestdist
 	public static void main(String[] args) {
 		Scanner shortdist = new Scanner(System.in);
 		double a1 = shortdist.nextDouble();double b1= shortdist.nextDouble();double a2=shortdist.nextDouble();double b2=shortdist.nextDouble();
-		
-		
 		System.out.println(Math.sqrt((b2 - b1) * (b2 - b1) + (a2 - a1) * (a2 - a1)));
 	}
 }
